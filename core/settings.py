dde1819b14bc9e8363a548450fd397dfc19917f4
sagarwal01 <74703552+sagarwal01@users.noreply.github.com>
2021-02-04 17:06:33 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -34,8 +34,8 @@ ALLOWED_HOSTS = ["insurance.herokuapp.com"]
 ALLOWED_HOSTS = ["*"]
 
 # Application definition
-STATIC_URL="/static/"
-STATIC_ROOT=os.path.join(BASE_DIR,"static")
+STATIC_URL="/static/assets/"
+STATIC_ROOT=os.path.join(BASE_DIR,"static/assets")
 
 
 INSTALLED_APPS = [
@@ -134,9 +134,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/static/assets'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'core/static'),
+    os.path.join(BASE_DIR, 'core/static/assets'),
 )
